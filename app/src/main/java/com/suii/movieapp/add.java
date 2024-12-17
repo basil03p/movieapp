5868e41e15a4c864b6f2ac9eb32172bd9b28681c
basil03p <basilmalikudy3@gmail.com>
2024-12-17 13:52:42 +0530

    page functionality added

--- app/src/main/java/com/suii/movieapp/add.java
+++ app/src/main/java/com/suii/movieapp/add.java
@@ -1,6 +1,9 @@
 package com.suii.movieapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +12,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class add extends AppCompatActivity {
-
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add);
+        b1=findViewById(R.id.back);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent po = new Intent(getApplicationContext(), Second.class);
+                startActivity(po);
+            }
+        });
     }
 }
